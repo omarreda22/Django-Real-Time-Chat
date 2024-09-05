4b4d92eeb01d1f74ff507c5b80504321de8ae6c4
Omar Reda <omar90180360@gmail.com>
2024-09-05 15:32:17 +0300

    Authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import home, user_login
+from .views import home, user_login, user_logout, user_register
 
 app_name = "accounts"
 
 urlpatterns = [
     path("", home, name="home"),
     path("login/", user_login, name="login"),
+    path("logout/", user_logout, name="logout"),
+    path("register/", user_register, name="register"),
 ]
